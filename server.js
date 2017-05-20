4c8f73f6bc780241acba95973318e3edfcfdbd5a
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2017-05-20 15:53:42 -0400

    Update server.js
    Comments added.

--- server.js
+++ server.js
@@ -64,7 +64,9 @@ function replyToSenderImage(sender, image) {
      });
 };
 
+// Function to check if input is numeric
 function isNumeric(n) {
+  
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
@@ -122,7 +124,7 @@ app.post('/webhook/', function (req, res) {
       }
 
       // Function to graph an equation
-      else if (action[0] == "graph"){
+      else if (action[0].toLowerCase() == "graph"){
 
         url = "https://www.graphsketch.com/render.php?eqn1_color=1&eqn1_eqn="+ action[1] +"&eqn2_color=2&eqn2_eqn=&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-17&x_max=17&y_min=-10.5&y_max=10.5&x_tick=1&y_tick=1&x_label_freq=5&y_label_freq=5&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525";
     
@@ -133,6 +135,10 @@ app.post('/webhook/', function (req, res) {
 
         apiai.on('response', (response) => {
           replyToSender(sender, response.result.fulfillment.speech);
+        });
+
+        apiai.on('error', (error) => {
+          console.log(error);
         });
 
         apiai.end();
