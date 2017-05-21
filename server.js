4d7f0ade1140151557881821182952fbc083abe1
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2017-05-20 21:47:36 -0400

    Error Checking
    Error Checking

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ var commands = "Here is a list of available commands:\
                 \ Derivative: equation\
                 \ Complex: equation\
                 "
+
 // Function to send plain message to the bot
 function replyToSender(sender, text) {
   messageData = {
@@ -80,6 +81,24 @@ function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+// Check if the string just have numbers of characters
+function validate(strValue) {
+
+  var objRegExp  = /^[a-zA-Z\u00C0-\u00ff\s]+$/;
+  return objRegExp.test(strValue);
+
+}
+
+// Check if character exists in a given string
+function checkVar(vari, char) {
+  return vari.indexOf(char) > -1;
+}
+
+// Check for unwanted errors from users
+function checkError(str, char) {
+  return (str[1].trim() == "" || validate(str[1]) == true && checkVar(str[1].trim(), char) == false);
+}
+
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === '=l]}NGdntMZMJQSE4qm5orPKq$x9Pf') {
     res.send(req.query['hub.challenge']);
@@ -105,42 +124,72 @@ app.post('/webhook/', function (req, res) {
         });
 
       // Function to simplify equations and fractions
-      if (action[0].toLowerCase() == "simplify") {
+      if (action[0].toLowerCase().trim() == "simplify") {
 
-        replyToSender(sender, "Answer Is: " + math.simplify(action[1]));
+        if (checkError(action, "x")) {
 
+          replyToSender(sender, "This is not allowed");
+
+        } else {
+
+          replyToSender(sender, "Answer Is: " + math.simplify(action[1]));
+
+        }
       
       }
 
       // Function to compute complex numbers
-      else if (action[0].toLowerCase() == "complex") {
+      else if (action[0].toLowerCase().trim() == "complex") {
+
+        if (checkError(action, "i")) {
+
+          replyToSender(sender, "This is not allowed");
+
+        } else {
 
         replyToSender(sender, "Answer Is: " + math.complex(action[1]));
 
       }
 
+    }
+
       // Function to get the derivative of an equation
-      else if (action[0].toLowerCase() == "derivative") {
+      else if (action[0].toLowerCase().trim() == "derivative") {
+
+        if (checkError(action, "x")) {
+
+          replyToSender(sender, "This is not allowed");
+
+        } else {
 
         replyToSender(sender, "Answer Is: " + math.derivative(action[1], "x"));
 
-      
       } 
+    }
 
       // Function to evaluate basic math operations
-      else if (isNumeric(text[0]) || text[0].toLowerCase() == "x" || text[0].toLowerCase() == "y") {
+      else if (isNumeric(text[0])) {
 
+        if (validate(text) == false) {
 
-        replyToSender(sender, "Answer Is: " + math.eval(text));
+          replyToSender(sender, "Answer Is: " + math.eval(text));
+        }
       }
 
       // Function to graph an equation
-      else if (action[0].toLowerCase() == "graph"){
+      else if (action[0].toLowerCase().trim() == "graph"){
 
-        url = "https://www.graphsketch.com/render.php?eqn1_color=1&eqn1_eqn="+ action[1].trim() +"&eqn2_color=2&eqn2_eqn=&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-17&x_max=17&y_min=-10.5&y_max=10.5&x_tick=1&y_tick=1&x_label_freq=5&y_label_freq=5&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525";
-    
-        replyToSender(sender, "Here is your graph for: " + action[1]);
-        replyToSenderImage(sender, url.trim());
+        if (checkError(action[1], "x")){
+
+          replyToSender(sender, "This is not allowed");
+
+        } else {
+
+          url = "https://www.graphsketch.com/render.php?eqn1_color=1&eqn1_eqn="+ action[1].trim() +"&eqn2_color=2&eqn2_eqn=&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-17&x_max=17&y_min=-10.5&y_max=10.5&x_tick=1&y_tick=1&x_label_freq=5&y_label_freq=5&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525";
+          replyToSender(sender, "Here is your graph for: " + action[1]);
+          replyToSenderImage(sender, url.trim());
+
+        }
       }
 
       // Function to get help with commands.
@@ -152,6 +201,7 @@ app.post('/webhook/', function (req, res) {
 
       else {
 
+        // Call function to connect to ai responses.
         apiai.on('response', (response) => {
           replyToSender(sender, response.result.fulfillment.speech);
         });
