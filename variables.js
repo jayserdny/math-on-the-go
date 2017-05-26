af5af5a45f1a765ed6c820ffd2e584866d8bfbc4
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2017-05-25 23:49:58 -0400

    Variables file to storage some long variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+module.exports = {
+	
+	getStartedEx: function() {
+		var greetingMessage = {
+    		"greeting":[
+        		{
+        			"locale":"default",
+        			"text":"Welcome to Math on the Fly! Solve your Math with me <3 !"
+        		}, 
+        		{
+        			"locale":"en_US",
+        			"text":"Welcome to Math on the Fly! Solve your Math with me <3 !"
+        		}
+    		]
+    	};
+
+    	return greetingMessage; 
+    }
+};
