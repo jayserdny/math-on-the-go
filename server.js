7c5a704da161b97a1b71bfaa29ec9ae1cecb0ff3
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2017-05-26 21:56:44 -0400

    console.log test removed

--- server.js
+++ server.js
@@ -1,468 +1,262 @@
-// Externals Libraries
-
-var app = require('express')();
-var bodyParser  = require('body-parser');
+var tools = require("./quickReplies.js");
 var request = require('request');
-var math = require('mathjs');
-var apiaiApp = require('apiai')(process.env.API_AI_API);
-var express = require('express');
-var request = require('request');
-var fs = require('file-system');
-var http = require('http');
-var download = require('download-file');
-var algebra = require('algebra.js');
-var Algebrite = require('algebrite');
-var menu = require("./menu.js");
 var variables = require("./variables.js");
-var functions = require("./functions.js");
-var path = require('path');
-var http = require("http");
-var port = process.env.PORT || 1881;
+var token = process.env.FACEBOOK_TOKEN;
+var menu = require("./menu.js");
+
+var self = module.exports = {
+
+	// Function to check if input is numeric
+	isNumeric: function(n) {
+
+  		return !isNaN(parseFloat(n)) && isFinite(n);
+	},
+
+	// Check if the string just have numbers of characters
+	validate: function(strValue) {
+  		var objRegExp  = /^[a-zA-Z\u00C0-\u00ff\s]+$/;
+  		return objRegExp.test(strValue);
+
+	},
+
+	// Check if character exists in a given string
+	checkVar: function(vari, char) {
+  		return vari.indexOf(char) > -1;
+	},
+
+	// Check for unwanted errors from users
+	checkError: function(str, char) {
+
+  		return (str[1].trim() == "" || self.validate(str[1]) == true && self.checkVar(str[1], char) == false);
+	},
+
+	// Check for unwanted errors when querying an image
+	checkErrorImg: function(str) {
+
+  		return (str[1].trim() == "" || self.validate(str[1]) == true);
+	},
+
+	// Convert integer to binary
+	toBinary: function(a) {
+  		return (a >>> 0).toString(2);
+	},
+
+	// Send POST to Facebook graph to load the quick replies
+	quickReplies: function(res, buttons) {
+
+  		request({
+
+    		url: 'https://graph.facebook.com/v2.6/me/messages',
+    		qs: { access_token : token },
+    		method: 'POST',
+    		headers: {'Content-Type': 'application/json'},
+    		form: buttons
+  		},
+  		function (error, response, body) {
+
+    	if (!error && response.statusCode == 200) {
+      	  console.log("ok");
+
+    	} else { 
+       	 console.log("error");
+    	}
+  		});
+	},
+
+	// Postback for quickreplies function
+	receivedPostback: function(res,event) {
+   		var senderID = event.sender.id;
+    	var recipientID = event.recipient.id;
+    	var timeOfMessage = event.timestamp;
+    	var payload = event.postback.payload;
+    	var replies = tools;
+
+    	switch(payload)
+    	{
+        case 'getstarted':
+            self.quickReplies(res, replies.getStartedEx(senderID)); 
+            break;
+
+        case 'GRAPH_COMMAND':
+            self.quickReplies(res, replies.graphEx(senderID)); 
+            break;
+
+        case 'SIMPLIFY_COMMAND':
+            self.quickReplies(res, replies.simplifyEx(senderID));
+            break;
+
+        case 'DERIVATE_COMMAND':
+            self.quickReplies(res, replies.derivateEx(senderID));
+            break;
+
+        case 'GCD_COMMAND':
+            self.quickReplies(res, replies.gcdEx(senderID));
+            break;
+
+        case 'XGCD_COMMAND':
+            self.quickReplies(res, replies.xgcdEx(senderID));
+            break;
+
+        case 'INTEGRAL_COMMAND':
+            self.quickReplies(res, replies.integralEx(senderID));
+            break;
+
+        case 'LCM_COMMAND':
+            self.quickReplies(res, replies.lcmEx(senderID));
+            break;
+
+        case 'SOLVE_COMMAND':
+            self.quickReplies(res, replies.solveEx(senderID));
+            break;
+
+        case 'TOBINARY_COMMAND':
+            self.quickReplies(res, replies.tobinaryEx(senderID));
+            break;
 
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+        default :
+            self.quickReplies(res, replies.getStartedEx(senderID));
+        break;
+    	}
 
-// Setup for express js
+	},
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+	// Function to send plain message to the bot
+	replyToSender: function(sender, text) {
+  		messageData = {
+   		 text : text
+  	};
 
-app.use(express.static(path.join(__dirname, 'private')));
-app.use('/public', express.static(path.join(__dirname + '/private')));
+  	request({
+    	url: 'https://graph.facebook.com/v2.6/me/messages',
+    	qs: { access_token : token },
+    	method: 'POST',
+    	json: {
+       		recipient: { id : sender },
+       		message: messageData,
+    	}
 
+ 	}, function(error, response, body) {
+       	if (error) {
 
-app.get('/setup',function(req,res){
+         	console.log('Error sending message: ', error);
 
-    functions.setupGetStartedButton(res);
-    functions.setupPersistentMenu(res);
-    functions.setupGreetingText(res);
-});
+       	} else if (response.body.error) {
 
+         	console.log('Error: ', response.body.error);
+       	}
+     	});
+	},
 
-// Function to replace all specific characters in a string
-String.prototype.replaceAll = function(str1, str2, ignore) {
+	// Function to retrive image from bot
+	replyToSenderImage: function(sender, image) {
 
-    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),
-    (ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-}
+  		request({
+    		url: 'https://graph.facebook.com/v2.6/me/messages',
+    		qs: { access_token : token },
+    		method: 'POST',
+    		json: {
+       			recipient: { id : sender },
+       			message: {
+        			attachment: {
+          				type: "image",
+          				payload: {
+            				url: image
+          				}
+        			}
+      			}
+    		}
 
+  		}, function(error, response, body) {
+       			if (error) {
 
-// Validate token from Facebook's webhooks.
-app.get('/webhook/', function (req, res) {
+         			console.log('Error sending message: ', error);
 
-  if (req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) { 
+       			} else if (response.body.error) {
 
-    res.send(req.query['hub.challenge']);
+         			console.log('Error: ', response.body.error);
+       			}
+     	});
+	},
 
-  }
+	// FACEBOOK SETUP AREA
 
-  res.send('Error, wrong validation token');
+	// Greeting message for first time interaction with the bot.
+	// TODO: Multi-locale
+	setupGreetingText: function(res){
 
-});
+  		request({
+    		url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+    		qs: { access_token : token },
+    		method: 'POST',
+    		headers: {'Content-Type': 'application/json'},
+    		form: variables.getStartedEx()
+  		},
+  		function (error, response, body) {
 
-app.post('/webhook/', function (req, res) {
+    		if (!error && response.statusCode == 200) {
+       		
+        		res.send(response.body);
 
-  messaging_events = req.body.entry[0].messaging;
+    		} else { 
+        		
+        		res.send(body);
+    		}
+  		});
 
-  for (i = 0; i < messaging_events.length; i++) {
+	},	
 
-    event = req.body.entry[0].messaging[i];
+	// Persistent Menu For Messenger
+	setupPersistentMenu: function(res){
 
-    sender = event.sender.id;
-
-    try {
-
-    image = event.message.attachments[0].payload.url;
-
-    if (image) {
-      //Checking if there are any image attachments 
-      if(event.message.attachments[0].type === "image"){
-
-        var imageURL = image;
-
-        var options = {
-          directory: "./private/",
-          filename: "image.png"
-        }
-
-        var results = "";
+		request({
+   			url: "https://graph.facebook.com/v2.6/me/messenger_profile",
+    		qs: { access_token : token },
+    		method: 'POST',
+    		headers: {'Content-Type': 'application/json'},
+    		form: menu.persistentMenu
+		},
+		function (error, response, body) {
+    		if (!error && response.statusCode == 200) {
  
-        download(imageURL, options, function(err){
+        		res.send(response);
 
-          if (err) {
+    		} else { 
 
-            throw err
+        		res.send(body);
+   			}
+		});
 
-          } else {
+	},
 
-            var url = "https://api.ocr.space/parse/imageurl?apikey="+ process.env.OCR_TOKEN + "&url=https://mathserver.herokuapp.com/public/image.png";
-            request({
-              url: url,
-              json: true
-              }, function (error, response, body) {
+	// Get started button setup
+	setupGetStartedButton: function(res){
+		var messageData = {
+        	"get_started":{
 
-                if (!error && response.statusCode === 200) {
+            	"payload":"getstarted"
+        	}
+		};
 
-                  try {
+		request({
+    		url: "https://graph.facebook.com/v2.6/me/messenger_profile",
+    		qs: { access_token : token },
+    		method: 'POST',
+    		headers: {'Content-Type': 'application/json'},
+    		form: messageData
+		},
+		function (error, response, body) {
 
-                    results = body.ParsedResults[0].ParsedText;
-                    var resultsStr = results.toString();
-                    var final = resultsStr.trim();
+    		if (!error && response.statusCode == 200) {
 
-                    try {
+       		 	res.send(response);
 
-                      functions.replyToSender(sender, "Answer Is: " + math.eval(final).replace(/\*/g, '%'));
-
-                    } catch (e) {
-
-                      functions.replyToSender(sender, "Please try a valid operation. Also, please try to use a photo without any other text other than basic math.");
-
-                      }
-
-                  } catch (e) { console.log(e)}
-                    
-                } else {
-                  console.log("error");
-                }
-              })
-            }
-          
-        });
- 
-      }
-    }
-
-    } catch(e){}
-    //Checking for attachments
+    		} else { 
     
-
-    if (event.message && event.message.text) {
-      
-      text = event.message.text;
-
-      action = event.message.text.split(":");
-
-      
-
-      apiai = apiaiApp.textRequest(text, {
-          sessionId: 'natural_language'
-        });
-      
-
-      // To simplify equations and fractions
-      if (action[0].toLowerCase().trim() == "simplify") {
-
-        try {
-
-          functions.replyToSender(sender, "Answer Is: " + Algebrite.simplify(action[1]).toString());
-
-        } catch(err){
-
-          functions.replyToSender(sender, "I can't simplify this :(. Please try the following one: 10/4")
-        }
-      
-      }
-
-      
-
-      // To compute complex numbers
-      else if (action[0].toLowerCase().trim() == "complex") {
-
-        try {
-
-          functions.replyToSender(sender, "Answer Is: " + math.complex(action[1]).toString());
-
-        } catch(err) {
-
-          functions.replyToSender(sender, "This is not a complex equation. Try this one: 2i + 5i");
-
-        }
-
-      }
-
-      // To calculate LCM from TWO numbers
-      else if (action[0].toLowerCase().trim() == "lcm") {
-
-        try {
-
-          input = action[1].trim();
-
-          lcm = input.split(",");
-          
-          functions.replyToSender(sender, "The LCM Is: " + math.lcm(lcm[0],lcm[1]).toString());
-
-        } catch(err) {
-
-          functions.replyToSender(sender, "Can't calculate the LCM with given values. Please, try the following: 4, 6")
-        }
-      }
-
-      // To calculate GCD from TWO numbers
-      else if (action[0].toLowerCase().trim() == "gcd") {
-
-        try {
-
-          txt = action[1].trim();
-
-          values = txt.split(",");
-
-          functions.replyToSender(sender, "The GCD Is: " + math.gcd(values[0],values[1]).toString());
-
-        } catch(err) {
-
-          functions.replyToSender(sender, "Can't calculate the GCD with given values. Please, try the following: 4, 6")
-        }
-      }
-
-      // To calculate Extended Euclidean algorithm from TWO numbers 
-      else if (action[0].toLowerCase().trim() == "xgcd") {
-
-        try {
-
-          txt = action[1].trim();
-
-          values = txt.split(",");
-
-          functions.replyToSender(sender, "The Extended Euclidean Algorithm Is: " + math.xgcd(values[0],values[1]).toString());
-
-        } catch(err) {
-
-          functions.replyToSender(sender, "Can't calculate the Extended Euclidean Algorithm with given values. Please, try the following: 4, 6")
-        }
-      }
-
-      // To convert units 
-      else if (action[0].toLowerCase().trim() == "convert") {
-
-        try {
-          
-          units = action[1].toString();
-          console.log(units);
-
-          functions.replyToSender(sender, "Answer Is: " + math.eval(units).toString());
-
-        } catch(err) {
-
-          functions.replyToSender(sender, "Can't convert this kind of unit. Please, try the following: 2 inch, cm")
-        }
-      }
-
-
-
-      // To get the derivative of an equation
-      else if (action[0].toLowerCase().trim() == "derivate") {
-
-          try {
-
-            functions.replyToSender(sender, "The Derivative Is: " + math.derivative(action[1].trim(), "x").toString());
-
-          } catch(err) {
-
-            functions.replyToSender(sender, "You can't derivate this function. Try this one: 2x^2 + 5x + 2")
-
-          }
-        
-      }
-
-      else if (action[0].toLowerCase().trim() == "integrate") {
-
-        try {
-
-          var integral = Algebrite.eval('integral(' + action[1].toString().trim() + ')').toString();
-          functions.replyToSender(sender, "The answer is: " + integral + " + C");
-        }
-
-        catch (err) {
-          console.log(err);
-          functions.replyToSender(sender, "error");
-        }
-      }
-
-      else if (action[0].toLowerCase().trim() == "defintegrate") {
-
-        var data = action[1].trim();
-        var data1 = data.split(",");
-        var equation = data1[0].toString();
-        var eqfinal = equation.trim();
-        var upper = data[1].trim();
-        var lower = data[2].trim();
-        console.log(data1);
-        console.log(equation);
-        x = "x";
-        y = "y";
-
-        try {
-          functions.replyToSender(sender, "The answer is: " + Algebrite.defint(eqfinal,y,0,x,lower,upper));
-        } catch (err) {
-
-          console.log(err);
-        }
-        
-      }
-
-      // To evaluate basic math operations
-      else if (functions.isNumeric(text[0])) {
-
-        try {
-
-          functions.replyToSender(sender, "Answer Is: " + math.eval(text).toString());
-
-        } catch(err) {
-
-          functions.replyToSender(sender, "I can't compute this equation. Instead, try this: 15 * 4 + 2");
-        }
-      }
-
-      // To graph an equation
-      else if (action[0].toLowerCase().trim() == "graph"){
-
-        if (functions.checkErrorImg(action)){
-
-          functions.replyToSender(sender, "This is not allowed");
-
-        } else {
-
-          var equation = action[1].replaceAll("+", "%2B");
-          var fEquation = equation.replaceAll("^", "%5E").trim();
-          var eq = fEquation.replaceAll("(", "%28").trim();
-          var eq1 = eq.replaceAll(")", "%29").trim();
-          var eq2 = eq1.replaceAll(" ", "%20").trim();
-
-          url = "https://www.graphsketch.com/render.php?eqn1_color=1&eqn1_eqn="+ eq2.trim() +"&x_min=-17&x_max=17&y_min=-10.5&y_max=10.5&x_tick=1&y_tick=1&x_label_freq=5&y_label_freq=5&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525";
-          functions.replyToSender(sender, "Here is your graph for: " + action[1]);
-          functions.replyToSenderImage(sender, url.trim());
-
-        }
-      }
-
-      else if (action[0].toLowerCase().trim() == "solve") {
-
-        try {
-
-          var eq = algebra.parse(action[1].trim());
-
-          var x = eq.solveFor("x");
-
-          functions.replyToSender(sender, "x = " + x.toString());          
-
-        } catch (err) {
-
-          functions.replyToSender(sender, "Error");
-        }
-      }
-
-      // To get help with commands.
-      else if (text.toLowerCase() == "help") {
-
-        functions.replyToSender(sender, variables.commands());
-
-      }
-
-      else if (action[0].toLowerCase().trim() == "tobinary") {
-
-        var integer = action[1].trim();
-        var binary = functions.toBinary(integer);
-
-        try {
-          
-          functions.replyToSender(sender, action[1] + " to binary is " + binary);
-
-        } catch (err) {
-
-          functions.replyToSender(sender, "Can't be converted");
-        }
-      }
-
-      else {
-
-        // Call function to connect to ai responses.
-        apiai.on('response', (response) => {
-          functions.replyToSender(sender, response.result.fulfillment.speech);
-        });
-
-        apiai.on('error', (error) => {
-          console.log(error);
-        });
-
-        apiai.end();
-      }
-
-    }
-
-    // PAYLOADS AREA - Controls the menu area
-
-    else if (event.postback && event.postback.payload) {
-
-          var payload = event.postback.payload;
-
-          switch (payload) {
-
-            case "getstarted":
-              functions.receivedPostback(res, event);
-              break;
-
-            case "GRAPH_COMMAND":
-              functions.receivedPostback(res, event);
-              break;
-
-            case "SIMPLIFY_COMMAND":
-              functions.receivedPostback(res, event);
-              break;
-
-            case "DERIVATE_COMMAND":
-              functions.receivedPostback(res, event);
-              break;
-
-            case "GCD_COMMAND":
-              functions.receivedPostback(res, event);
-              break;
-
-            case "XGCD_COMMAND":
-              functions.receivedPostback(res, event);
-              break;
-
-            case "INTEGRAL_COMMAND":
-              functions.receivedPostback(res, event);
-              break;
-
-            case 'LCM_COMMAND':
-              functions.receivedPostback(res, event);
-              break;
-
-            case 'SOLVE_COMMAND':
-              functions.receivedPostback(res, event);
-              break;
-
-            case 'TOBINARY_COMMAND':
-              functions.receivedPostback(res, event);
-              break;
-
-            default:
-              functions.receivedPostback(res, event);
-              
-            break; 
-          }
-            
-    }
-         
-          
-  }
-  
-
-  res.sendStatus(200);
-
-});
-
-
-
-app.get('/', function (req, res) {
-  res.sendStatus(200);
-});
-
-app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
-});
+        		res.send(body);
+    		}
+		});
+	}
+
+	// FACEBOOK SETUP END
+};
